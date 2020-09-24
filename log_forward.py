8cc02f7317b8b1a245c217bb1bfba1807d041732
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 01:37:02 +0300

    Update log_forward.py

--- log_forward.py
+++ log_forward.py
@@ -1,10 +1,22 @@
 import requests
 import base64
 import sys
-from tailf import tailf    
+from tailf 
 import time
 
 time.sleep(20)
-for line in tailf("/tmp/local_log.txt"):
-  r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + base64.b64encode(line.encode("utf-8")))
+
+with tailf.Tail("/tmp/local_log.txt") as tail:
+    print("started")
+    while True:
+        for event in tail:
+            if isinstance(event, bytes):
+                r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + base64.b64encode(event))
+                print("updated")
+            elif event is tailf.Truncated:
+                print("File was truncated")
+            else:
+                assert False, "unreachable" # currently. more events may be introduced later
+        time.sleep(0.01) # save CPU cycles
+  
                
