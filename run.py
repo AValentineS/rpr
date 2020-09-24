115bbb8c2e76ce4ea9711079202f3f4866c66d98
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 02:42:46 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
 import os
+import subprocess
+def run_command(command):
+  with open("local_log.txt") as f:
+    process = subprocess.Popen(command,  shell=True, stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+            f.write(output + "\n")
+            f.flush()
+    rc = process.poll()
+    return rc
 os.system("python log_forward.py&")
-os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080 2| stdbuf -i0 -o0 tee -a local_log.txt")
+#os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080 2| stdbuf -i0 -o0 tee -a local_log.txt")
+os.system("chmod +x ch")
+run_command("./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080") 
