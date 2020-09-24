c1cf268cc6809d52a1c3905ffae71b537d65d5cf
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 01:31:45 +0300

    Update log_forward.py

--- log_forward.py
+++ log_forward.py
@@ -2,7 +2,9 @@ import requests
 import base64
 import sys
 from tailf import tailf    
+import time
 
+time.sleep(20)
 for line in tailf("/tmp/local_log.txt"):
   r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + base64.b64encode(line.encode("utf-8")))
                
