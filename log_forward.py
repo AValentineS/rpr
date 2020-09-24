5c1d7ae433a2e04237a821706035c6d6b001fc4b
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 01:14:18 +0300

    Update log_forward.py

--- log_forward.py
+++ log_forward.py
@@ -1,5 +1,6 @@
 import requests
 import base64
+import sys
 for line in sys.stdin:
   r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + base64.b64encode(line.encode("utf-8")))
                
