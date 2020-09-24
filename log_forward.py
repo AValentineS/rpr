ce5d854bcfd5eebf0cda280600146a07338905f6
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 02:22:14 +0300

    Update log_forward.py

--- log_forward.py
+++ log_forward.py
@@ -14,9 +14,10 @@ while True:
     r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + str(base64.b64encode(line)))
     if size!=oldsize:
         with open(fn, "r") as f:
-            if size>oldsize
+            if size>oldsize:
                 f.seek(oldsize)
-                line = f.readline()
+            for line in f:
                 r = requests.get('http://127.0.0.1:8080/tfpw.php?data=' + str(base64.b64encode(line)))
     oldsize = size
+    time.sleep(2.5)
             
