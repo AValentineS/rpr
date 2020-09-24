f857d7c908e762667f0966beac085f0a971fef37
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 02:30:02 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 import os
 os.system("python log_forward.py&")
-os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080 | stdbuf -i0 -o0 tee -a local_log.txt")
+os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080 2| stdbuf -i0 -o0 tee -a local_log.txt")
