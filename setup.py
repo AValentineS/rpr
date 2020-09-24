dba7e4073e8b7943f28f03fbc129d2d4a73e714b
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-24 17:14:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,3 @@
 import os
-os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --proxy 127.0.0.1:8080")
+#os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --proxy 127.0.0.1:8080")
+print("setup: OK")
