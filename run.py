7e9b07f01b1f3e59270a10b43afbd355296d6c03
AValentineS <71728353+AValentineS@users.noreply.github.com>
2020-09-25 03:01:55 +0300

    Update run.py

--- run.py
+++ run.py
@@ -22,6 +22,7 @@ def run_command(command):
     rc = process.poll()
     return rc
 #os.system("python log_forward.py&")
-#os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080 2| stdbuf -i0 -o0 tee -a local_log.txt")
-os.system("chmod +x ch")
-run_command("./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080") 
+os.system("chmod +x ch && ./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080")
+# 2| stdbuf -i0 -o0 tee -a local_log.txt
+#os.system("chmod +x ch")
+#run_command("./ch server --port $PORT --auth $C_AUTH --reverse --backend http://127.0.0.1:8080") 
